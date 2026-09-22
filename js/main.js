78eddab334292e915962be9e58535e9495f34246
elnacime10 <47749041+elnacime10@users.noreply.github.com>
2026-09-22 10:39:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ refreshSet();
 refreshSetup();
 
 /* ---- Numéro de version : sur l'accueil et dans la pause ---- */
-const VERSION_JEU = '1.4';
+const VERSION_JEU = '1.5';
 $('#versionHome').textContent = 'Version ' + VERSION_JEU;
 $('#versionMenu').textContent = 'Version ' + VERSION_JEU;
 
